b09f39c2c9631453e407dbb83c85663c15ac26ed
Noah Kim <noah.bjong.kim@gmail.com>
2023-12-13 17:31:29 +0100

    Distribution histograms and Regressions
    Distribution hists for subregions
    Scatterplots for the socio economic indicators
    basic linear model as function for regression

--- Code/main.R
+++ Code/main.R
@@ -2,19 +2,19 @@ library(dplyr)
 library(tidyverse)
 library(tidyr)
 library(ltm)
-
-setwd('/Users/noahkim/Documents/Studium/ISTP/Stats 1/Groupwork/StatsGroupwork/')
-directory <- getwd()
-
-#filter data and create datasets East_Ger, West_Ger and Berlin
-source('filter_Data.R')
-
 # N for DE: 2358
 # N >= 18: 2273
 # N for West_Ger: 1807
 # N for East_Ger: 376
 # N for Berlin: 90
 
+setwd('/Users/noahkim/Documents/Studium/ISTP/Stats 1/Groupwork/StatsGroupwork/')
+directory <- getwd()
+
+#filter data and create datasets East_Ger, West_Ger and Berlin
+source('Code/filter_Data.R')
+
+############################# Fuctions
 plot_distribution_histograms <- function(dataframe, name_index) {
   # Plot histograms of individual trust indicators to see the distribution
   file_directory <- paste0(directory, "/Distribution histograms/")
@@ -23,7 +23,7 @@ plot_distribution_histograms <- function(dataframe, name_index) {
     labs(title="Trust in politicians",
          x = "Response",
          y = "Count")
-  file_name = paste0(file_directory, name_index," Trust in politicians.png" )
+  file_name = paste0(file_directory,"Trust in politicians ",name_index,".png" )
   ggsave(file_name, width = 6, height = 5)
   
   ggplot(dataframe, aes(x = factor(trstprt))) + 
@@ -31,7 +31,7 @@ plot_distribution_histograms <- function(dataframe, name_index) {
     labs(title="Trust in parties",
          x = "Response",
          y = "Count")
-  file_name = paste0(file_directory, name_index," Trust in parties.png" )
+  file_name = paste0(file_directory,"Trust in parties ",name_index,".png" )
   ggsave(file_name, width = 6, height = 5)  
   
   ggplot(dataframe, aes(x = factor(trstprl))) + 
@@ -39,7 +39,7 @@ plot_distribution_histograms <- function(dataframe, name_index) {
     labs(title = "Trust in parliament",
          x = "Response",
          y = "Count")
-  file_name = paste0(file_directory, name_index," Trust in parliament.png" )
+  file_name = paste0(file_directory,"Trust in parliament ",name_index,".png" )
   ggsave(file_name, width = 6, height = 5)  
   
   # Compute Cronbach's Alpha (result = 0.884, so quite good!)
@@ -60,15 +60,106 @@ plot_distribution_histograms <- function(dataframe, name_index) {
     labs(title = paste0("Distribution of responses to questions of trust in government, c_alpha =",c_alpha),
          x = "Response",
          y = "Count")
-  file_name = paste0(file_directory, name_index," Total Trust.png" )
-  ggsave(file_name, width = 7, height = 5)}
+  file_name = paste0(file_directory,"Total Trust ",name_index,".png" )
+  ggsave(file_name, width = 8, height = 5)
+  }
 
-# All of Germany:
+plot_scatterplots <- function(dataframe, name_index){
+  file_directory <- paste0(directory, "/Scatterplots/")
+
+  # Create a dataframe with the frequency of each value in the 'x' column
+  frequency_data <- dataframe %>%
+    group_by(polintr,total_trust) %>%
+    summarize(freq = n())
+  # Using ggplot2 with size based on frequency
+  ggplot(frequency_data, aes(x = polintr, y = total_trust, size = freq)) +
+    geom_point() +
+    labs(title = "Scatterplot with Point Size based on Frequency", x = "Political Interest", y = "Total Trust") +
+    scale_size_continuous(range = c(1, 8))  # Adjust the size range as needed
+  file_name = paste0(file_directory,"Political Interest Scatterplot ",name_index,".png" )
+  ggsave(file_name, width = 8, height = 5)
+  
+  # Create a dataframe with the frequency of each value in the 'x' column
+  frequency_data <- dataframe %>%
+    group_by(prtvede2,total_trust) %>%
+    summarize(freq = n())
+  # Using ggplot2 with size based on frequency
+  ggplot(frequency_data, aes(x = prtvede2, y = total_trust, size = freq)) +
+    geom_point() +
+    labs(title = "Scatterplot with Point Size based on Frequency", x = "Party affiliation", y = "Total Trust") +
+    scale_size_continuous(range = c(1, 8))  # Adjust the size range as needed
+  file_name = paste0(file_directory,"Party Affiliation Scatterplot ",name_index,".png" )
+  ggsave(file_name, width = 8, height = 5)
+  
+  # Create a dataframe with the frequency of each value in the 'x' column
+  frequency_data <- dataframe %>%
+    group_by(agea,total_trust) %>%
+    summarize(freq = n())
+  # Using ggplot2 with size based on frequency
+  ggplot(frequency_data, aes(x = agea, y = total_trust, size = freq)) +
+    geom_point() +
+    labs(title = "Scatterplot with Point Size based on Frequency", x = "Age", y = "Total Trust") +
+    scale_size_continuous(range = c(1, 8))  # Adjust the size range as needed
+  file_name = paste0(file_directory,"Age Scatterplot ",name_index,".png" )
+  ggsave(file_name, width = 8, height = 5)
+  
+  # Create a dataframe with the frequency of each value in the 'x' column
+  frequency_data <- dataframe %>%
+    group_by(hinctnta,total_trust) %>%
+    summarize(freq = n())
+  # Using ggplot2 with size based on frequency
+  ggplot(frequency_data, aes(x = hinctnta, y = total_trust, size = freq)) +
+    geom_point() +
+    labs(title = "Scatterplot with Point Size based on Frequency", x = "Household Income", y = "Total Trust") +
+    scale_size_continuous(range = c(1, 8))  # Adjust the size range as needed
+  file_name = paste0(file_directory,"Household Income Scatterplot ",name_index,".png" )
+  ggsave(file_name, width = 8, height = 5)
+}
+
+trust_regression_model <- function(dataframe){
+  # Create a formula for the regression
+  formula <- total_trust ~ hinctnta + polintr + prtvede2 + agea
+  
+  # Fit the multivariate regression model
+  model <- lm(formula,dataframe)
+  
+  # Print the summary of the regression model
+  model_summary <- summary(model)
+  
+  # Extract the r squared value
+  r_squared_value <- model_summary$r.squared
+  
+  return(r_squared_value)
+}
+
+############################# Data Subset Creation
 # We can filter out all the rows which have one or more NaNs in trstplr, trstplt, trstprt
-filt_trust <- Data_DE %>% filter_at(vars(trstprl, trstplt, trstprt), all_vars(!is.na(.)))
+Whole_DE_filt_trust <- Data_DE %>% filter_at(vars(trstprl, trstplt, trstprt, polintr, prtvede2, hinctnta, agea), all_vars(!is.na(.)))
 
 # Stack the trust variables
-filt_trust <- filt_trust %>%
+Whole_DE_filt_trust <- Whole_DE_filt_trust %>%
   mutate(total_trust = trstprl + trstplt + trstprt)
 
-plot_distribution_histograms(filt_trust, "Whole Germany")
+# Create regional Subsets
+East_DE_filt_trust <- Whole_DE_filt_trust %>% filter(EastWest == "East")
+West_DE_filt_trust <- Whole_DE_filt_trust %>% filter(EastWest == "West")
+Berlin_DE_filt_trust <- Whole_DE_filt_trust %>% filter(EastWest == "Berlin")
+
+############################# Distribution histograms/stats
+# # Plot distributions of the individual and stacked trust variables
+# plot_distribution_histograms(Whole_DE_filt_trust, "Whole Germany")
+# plot_distribution_histograms(East_DE_filt_trust, "East Germany")
+# plot_distribution_histograms(West_DE_filt_trust, "West Germany")
+# plot_distribution_histograms(Berlin_DE_filt_trust, "Berlin")
+# 
+# # Print summaries of the statistics of the total trust variable: East Germany has indeed lower trust
+# print(summary(Whole_DE_filt_trust$total_trust))
+# print(summary(West_DE_filt_trust$total_trust))
+# print(summary(East_DE_filt_trust$total_trust))
+# print(summary(Berlin_DE_filt_trust$total_trust))
+
+# ############################ Scatterplots
+# plot_scatterplots(Whole_DE_filt_trust, "Whole Germany")
+
+############################# Regression
+
